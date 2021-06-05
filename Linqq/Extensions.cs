f92d61d3f28345035345d3aeb4ddb470c7dfe18d
NigelBess <nigelbess@gmail.com>
2021-06-05 10:06:08 -0700

    first commit

---
+++ Linqq/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Linqq
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> Enumerate<T>(this T[,] array)
+        {
+            foreach (var item in array)
+            {
+                yield return item;
+            }
+        }
+    }
+}
